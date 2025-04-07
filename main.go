e118b73186edb620ec086eb485e02daa9794b1c6
Prokhor Sevastianov <p.sevastianov@propellerads.net>
2025-04-07 22:53:46 +0200

    sum

--- main.go
+++ main.go
@@ -1,43 +1,65 @@
 package main
 
 import (
+	"math"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
-var courses = map[int64]string{
-	1: "Introduction to programming",
-	2: "Introduction to algorithms",
-	3: "Data structures",
-}
-
 func main() {
+	cwd, _ := os.Getwd()
+	logFile := filepath.Join(cwd, ".log")
+	logger := logrus.New()
+	logger.SetOutput(os.Stdout)
+	file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer file.Close()
+	logger.SetOutput(file)
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", IndexHandler)
-	mux.HandleFunc("/courses/description", CourseDescHandler)
 
 	server := &http.Server{
-		Addr:              ":80",
+		Addr:              ":8080",
 		Handler:           mux,
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
-}
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Go to /sum"))
+	})
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Go to /courses/description"))
-}
+	mux.HandleFunc("/sum", func(w http.ResponseWriter, r *http.Request) {
+		x, err := strconv.Atoi(r.URL.Query().Get("x"))
+		if err != nil {
+			panic(err)
+		}
+		y, err := strconv.Atoi(r.URL.Query().Get("y"))
+		if err != nil {
+			panic(err)
+		}
+		if (y > 0 && x > math.MaxInt-y) || (y < 0 && x < math.MinInt-y) {
+			logger.WithFields(logrus.Fields{
+				"x": x,
+				"y": y,
+			}).Warning("Sum overflows int")
+			w.Write([]byte("-1"))
+			return
+		}
+		sum := x + y
+		w.Write([]byte(strconv.Itoa(sum)))
+	})
 
-func CourseDescHandler(w http.ResponseWriter, r *http.Request) {
-	cId := r.URL.Query().Get("course_id")
-	id, err := strconv.ParseInt(cId, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	w.Write([]byte(courses[id]))
+	port := "8080"
+	logWithPort := logrus.WithFields(logrus.Fields{
+		"port": port,
+	})
+	logWithPort.Info("Starting a web-server on port")
+	logWithPort.Fatal(server.ListenAndServe())
 }
--- main_test.go
+++ main_test.go
@@ -1,49 +1,99 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"math"
 	"net/http"
-	"net/http/httptest"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestPractice(t *testing.T) {
+	r := require.New(t)
+	// Check that we can't run a new process on the same port.
+	runOutput2 := bytes.NewBuffer(nil)
+	runCmd2 := exec.Command("go", "run", "main.go")
+	runCmd2.Stdout = runOutput2
+	runCmd2.Stderr = runOutput2
+	r.Error(runCmd2.Run())
+	r.Contains(runOutput2.String(), "listen tcp :8080: bind: address already in use")
+
+	// Run test cases.
 	testCases := []struct {
-		name     string
-		courseID string
-		want     string
+		name                 string
+		x                    int
+		y                    int
+		expectedResponseBody string
+		expectedCode         int
+		expectedLog          string
 	}{
 		{
-			name:     "course 1",
-			courseID: "1",
-			want:     "Introduction to programming",
+			name:                 "positive",
+			x:                    11,
+			y:                    55,
+			expectedResponseBody: "66",
+			expectedCode:         http.StatusOK,
+			expectedLog:          "",
 		},
 		{
-			name:     "course 2",
-			courseID: "2",
-			want:     "Introduction to algorithms",
+			name:                 "positive #2",
+			x:                    124,
+			y:                    77777,
+			expectedResponseBody: "77901",
+			expectedCode:         http.StatusOK,
+			expectedLog:          "",
 		},
 		{
-			name:     "course 3",
-			courseID: "3",
-			want:     "Data structures",
+			name:                 "overflow with bigger x",
+			x:                    math.MaxInt - 10,
+			y:                    700,
+			expectedResponseBody: "-1",
+			expectedCode:         http.StatusOK,
+			expectedLog:          fmt.Sprintf("level=warning msg=\"Sum overflows int\" x=%d y=700", math.MaxInt-10),
+		},
+		{
+			name:                 "overflow with bigger x",
+			x:                    500,
+			y:                    math.MaxInt - 10,
+			expectedResponseBody: "-1",
+			expectedCode:         http.StatusOK,
+			expectedLog:          fmt.Sprintf("level=warning msg=\"Sum overflows int\" x=500 y=%d", math.MaxInt-10),
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			httpRequest, err := http.NewRequest("GET", "/courses/description?course_id="+tc.courseID, nil)
-			r := require.New(t)
-			r.NoError(err)
+			req, tErr := http.NewRequest(
+				http.MethodGet,
+				fmt.Sprintf("http://localhost:8080/sum?x=%d&y=%d", tc.x, tc.y),
+				nil,
+			)
+			tr := require.New(t)
+			tr.NoError(tErr)
 
-			httpRecorder := httptest.NewRecorder()
+			httpClient := http.Client{}
+			resp, tErr := httpClient.Do(req)
+			tr.NoError(tErr)
+			defer resp.Body.Close()
 
-			handler := http.HandlerFunc(CourseDescHandler)
-			handler.ServeHTTP(httpRecorder, httpRequest)
+			tr.Equal(tc.expectedCode, resp.StatusCode)
+			if tc.expectedResponseBody != "" {
+				body, rErr := io.ReadAll(resp.Body)
+				tr.NoError(rErr)
+				tr.Equal(tc.expectedResponseBody, string(body))
+			}
 
-			r.Equal(http.StatusOK, httpRecorder.Code)
-			r.Equal(tc.want, httpRecorder.Body.String())
+			dat, _ := os.ReadFile(".log")
+			log := string(dat)
+			fmt.Print(log)
+			if tc.expectedLog != "" {
+				tr.Contains(log, tc.expectedLog)
+			}
 		})
 	}
 }
