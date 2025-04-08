507c6b2a7a953684ced1454ed5d45f673b794d39
Prokhor Sevastianov <p.sevastianov@propellerads.net>
2025-04-08 17:48:25 +0200

    currency

--- main.go
+++ main.go
@@ -1,65 +1,33 @@
 package main
 
 import (
-	"math"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
-	"time"
+	"fmt"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
+var exchangeRate = map[string]float64{
+	"USD/EUR": 0.8,
+	"EUR/USD": 1.25,
+	"USD/GBP": 0.7,
+	"GBP/USD": 1.43,
+	"USD/JPY": 110,
+	"JPY/USD": 0.0091,
+}
+
 func main() {
-	cwd, _ := os.Getwd()
-	logFile := filepath.Join(cwd, ".log")
-	logger := logrus.New()
-	logger.SetOutput(os.Stdout)
-	file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	defer file.Close()
-	logger.SetOutput(file)
+	webApp := fiber.New()
+	webApp.Get("/convert", func(c *fiber.Ctx) error {
+		from := c.Query("from")
+		to := c.Query("to")
+		key := from + "/" + to
+		if rate, ok := exchangeRate[key]; ok {
+			return c.SendString(fmt.Sprintf("%.2f", rate))
+		}
 
-	mux := http.NewServeMux()
-
-	server := &http.Server{
-		Addr:              ":8080",
-		Handler:           mux,
-		ReadHeaderTimeout: 10 * time.Second,
-	}
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Go to /sum"))
+		return c.Status(fiber.StatusNotFound).SendString("Not Found")
 	})
 
-	mux.HandleFunc("/sum", func(w http.ResponseWriter, r *http.Request) {
-		x, err := strconv.Atoi(r.URL.Query().Get("x"))
-		if err != nil {
-			panic(err)
-		}
-		y, err := strconv.Atoi(r.URL.Query().Get("y"))
-		if err != nil {
-			panic(err)
-		}
-		if (y > 0 && x > math.MaxInt-y) || (y < 0 && x < math.MinInt-y) {
-			logger.WithFields(logrus.Fields{
-				"x": x,
-				"y": y,
-			}).Warning("Sum overflows int")
-			w.Write([]byte("-1"))
-			return
-		}
-		sum := x + y
-		w.Write([]byte(strconv.Itoa(sum)))
-	})
-
-	port := "8080"
-	logWithPort := logrus.WithFields(logrus.Fields{
-		"port": port,
-	})
-	logWithPort.Info("Starting a web-server on port")
-	logWithPort.Fatal(server.ListenAndServe())
+	logrus.Fatal(webApp.Listen(":8080"))
 }
--- main_test.go
+++ main_test.go
@@ -1,68 +1,49 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
-	"math"
 	"net/http"
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestPractice(t *testing.T) {
-	r := require.New(t)
-	// Check that we can't run a new process on the same port.
-	runOutput2 := bytes.NewBuffer(nil)
-	runCmd2 := exec.Command("go", "run", "main.go")
-	runCmd2.Stdout = runOutput2
-	runCmd2.Stderr = runOutput2
-	r.Error(runCmd2.Run())
-	r.Contains(runOutput2.String(), "listen tcp :8080: bind: address already in use")
-
-	// Run test cases.
 	testCases := []struct {
-		name                 string
-		x                    int
-		y                    int
-		expectedResponseBody string
-		expectedCode         int
-		expectedLog          string
+		name     string
+		from     string
+		to       string
+		wantCode int
+		want     string
 	}{
 		{
-			name:                 "positive",
-			x:                    11,
-			y:                    55,
-			expectedResponseBody: "66",
-			expectedCode:         http.StatusOK,
-			expectedLog:          "",
+			name:     "exchange rate not found 1",
+			from:     "USD",
+			to:       "GEL",
+			wantCode: http.StatusNotFound,
+			want:     "",
 		},
 		{
-			name:                 "positive #2",
-			x:                    124,
-			y:                    77777,
-			expectedResponseBody: "77901",
-			expectedCode:         http.StatusOK,
-			expectedLog:          "",
+			name:     "exchange rate not found 2",
+			from:     "GEL",
+			to:       "USD",
+			wantCode: http.StatusNotFound,
+			want:     "",
 		},
 		{
-			name:                 "overflow with bigger x",
-			x:                    math.MaxInt - 10,
-			y:                    700,
-			expectedResponseBody: "-1",
-			expectedCode:         http.StatusOK,
-			expectedLog:          fmt.Sprintf("level=warning msg=\"Sum overflows int\" x=%d y=700", math.MaxInt-10),
+			name:     "positive 1",
+			from:     "EUR",
+			to:       "USD",
+			wantCode: http.StatusOK,
+			want:     "1.25",
 		},
 		{
-			name:                 "overflow with bigger x",
-			x:                    500,
-			y:                    math.MaxInt - 10,
-			expectedResponseBody: "-1",
-			expectedCode:         http.StatusOK,
-			expectedLog:          fmt.Sprintf("level=warning msg=\"Sum overflows int\" x=500 y=%d", math.MaxInt-10),
+			name:     "positive 2",
+			from:     "USD",
+			to:       "JPY",
+			wantCode: http.StatusOK,
+			want:     "110.00",
 		},
 	}
 
@@ -70,7 +51,7 @@ func TestPractice(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			req, tErr := http.NewRequest(
 				http.MethodGet,
-				fmt.Sprintf("http://localhost:8080/sum?x=%d&y=%d", tc.x, tc.y),
+				fmt.Sprintf("http://localhost:8080/convert?from=%s&to=%s", tc.from, tc.to),
 				nil,
 			)
 			tr := require.New(t)
@@ -81,18 +62,11 @@ func TestPractice(t *testing.T) {
 			tr.NoError(tErr)
 			defer resp.Body.Close()
 
-			tr.Equal(tc.expectedCode, resp.StatusCode)
-			if tc.expectedResponseBody != "" {
+			tr.Equal(tc.wantCode, resp.StatusCode)
+			if tc.want != "" {
 				body, rErr := io.ReadAll(resp.Body)
 				tr.NoError(rErr)
-				tr.Equal(tc.expectedResponseBody, string(body))
-			}
-
-			dat, _ := os.ReadFile(".log")
-			log := string(dat)
-			fmt.Print(log)
-			if tc.expectedLog != "" {
-				tr.Contains(log, tc.expectedLog)
+				tr.Equal(tc.want, string(body))
 			}
 		})
 	}
