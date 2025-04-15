b65f4df1b45765daf03eb1fbdeabb0e8478b30e3
Prokhor Sevastianov <p.sevastianov@propellerads.net>
2025-04-15 16:56:27 +0200

    json

--- main.go
+++ main.go
@@ -1,36 +1,52 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
+	"slices"
 )
 
-var postLikes = map[string]int64{}
+type (
+	BinarySearchRequest struct {
+		Numbers []int `json:"numbers"`
+		Target  int   `json:"target"`
+	}
+
+	BinarySearchResponse struct {
+		TargetIndex int    `json:"target_index"`
+		Error       string `json:"error,omitempty"`
+	}
+)
+
+const targetNotFound = -1
 
 func main() {
 	webApp := fiber.New(fiber.Config{
-		Immutable:      true,
 		ReadBufferSize: 16 * 1024})
 	webApp.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Go to /likes/12345")
+		return c.SendStatus(200)
 	})
-	webApp.Get("/likes/:post_id", func(c *fiber.Ctx) error {
-		postId := c.Params("post_id", "")
-		if likes, ok := postLikes[postId]; ok {
-			return c.SendString(strconv.Itoa(int(likes)))
+
+	webApp.Post("/search", func(c *fiber.Ctx) error {
+		var request BinarySearchRequest
+		if err := c.BodyParser(&request); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(BinarySearchResponse{
+				TargetIndex: -1,
+				Error:       "Invalid JSON",
+			})
 		}
-		return c.Status(fiber.StatusNotFound).SendString("Not Found")
-	})
-	webApp.Post("/likes/:post_id", func(c *fiber.Ctx) error {
-		postId := c.Params("post_id", "")
-		if _, ok := postLikes[postId]; ok {
-			postLikes[postId] = postLikes[postId] + 1
-			return c.SendString(strconv.Itoa(int(postLikes[postId])))
+		targetIndex := slices.Index(request.Numbers, request.Target)
+		if targetIndex == targetNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(BinarySearchResponse{
+				TargetIndex: -1,
+				Error:       "Target was not found",
+			})
 		}
-		postLikes[postId] = 1
-		return c.Status(fiber.StatusCreated).SendString(strconv.Itoa(int(postLikes[postId])))
+
+		return c.JSON(BinarySearchResponse{
+			TargetIndex: targetIndex,
+			Error:       "",
+		})
 	})
 
 	logrus.Fatal(webApp.Listen(":8080"))
--- main_test.go
+++ main_test.go
@@ -1,74 +1,92 @@
-package main
+package main_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/require"
+)
+
+type (
+	BinarySearchResponse struct {
+		TargetIndex int    `json:"target_index"`
+		Error       string `json:"error,omitempty"`
+	}
 )
 
 func TestPractice(t *testing.T) {
 	testCases := []struct {
-		name          string
-		postID        string
-		requestMethod string
-		wantCode      int
-		want          string
+		name        string
+		requestBody string
+		wantCode    int
+		wantIndex   int
+		wantError   string
 	}{
 		{
-			name:          "post not found",
-			postID:        "12345",
-			requestMethod: fiber.MethodGet,
-			wantCode:      http.StatusNotFound,
-			want:          "",
+			name:        "invalid JSON request body",
+			requestBody: `{"numbe`,
+			wantCode:    http.StatusBadRequest,
+			wantIndex:   -1,
+			wantError:   "Invalid JSON",
 		},
 		{
-			name:          "post increment 1",
-			postID:        "12345",
-			requestMethod: fiber.MethodPost,
-			wantCode:      http.StatusCreated,
-			want:          "1",
+			name:        "target number is not found",
+			requestBody: `{"numbers": [1, 2, 3, 5], "target": 4}`,
+			wantCode:    http.StatusNotFound,
+			wantIndex:   -1,
+			wantError:   "Target was not found",
 		},
 		{
-			name:          "post increment 2",
-			postID:        "12345",
-			requestMethod: fiber.MethodPost,
-			wantCode:      http.StatusOK,
-			want:          "2",
+			name:        "numbers count is even. Target number is found",
+			requestBody: `{"numbers": [1, 2, 3, 7, 99, 100, 250, 1000], "target": 99}`,
+			wantCode:    http.StatusOK,
+			wantIndex:   4,
 		},
 		{
-			name:          "get incremented post",
-			postID:        "12345",
-			requestMethod: fiber.MethodGet,
-			wantCode:      http.StatusOK,
-			want:          "2",
+			name:        "numbers count is odd. Target number is found",
+			requestBody: `{"numbers": [1, 2, 7, 99, 100, 250, 1000], "target": 99}`,
+			wantCode:    http.StatusOK,
+			wantIndex:   3,
 		},
 	}
 
+	runOutput := bytes.NewBuffer(nil)
+
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			defer fmt.Printf("Web-app output: %s\n", runOutput.String())
+
 			req, tErr := http.NewRequest(
-				tc.requestMethod,
-				fmt.Sprintf("http://localhost:8080/likes/%s", tc.postID),
-				nil,
+				http.MethodPost,
+				"http://localhost:8080/search",
+				strings.NewReader(tc.requestBody),
 			)
 			tr := require.New(t)
 			tr.NoError(tErr)
+
+			req.Header.Set("Content-Type", "application/json")
 
 			httpClient := http.Client{}
 			resp, tErr := httpClient.Do(req)
 			tr.NoError(tErr)
 			defer resp.Body.Close()
 
+			bodyBytes, tErr := io.ReadAll(resp.Body)
+			tr.NoError(tErr)
+
 			tr.Equal(tc.wantCode, resp.StatusCode)
-			if tc.want != "" {
-				body, rErr := io.ReadAll(resp.Body)
-				tr.NoError(rErr)
-				tr.Equal(tc.want, string(body))
-			}
+
+			bsResp := BinarySearchResponse{}
+			tErr = json.Unmarshal(bodyBytes, &bsResp)
+			tr.NoError(tErr)
+
+			tr.Equal(tc.wantIndex, bsResp.TargetIndex)
+			tr.Equal(tc.wantError, bsResp.Error)
 		})
 	}
 }
